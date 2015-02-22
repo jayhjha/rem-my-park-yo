8bc857d408ae49b50110c01801ba1bae47fb7a3b
Himanshu Jha <himanshujha19@gmail.com>
2015-02-21 21:46:10 -0500

    Added a subdomain to listen to incoming Yo messages and find the username.
    For this implementation, a Yo is sent out right after one is received, to the same user.

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 var express = require('express')
+var request = require('request')
 var app = express();
+
+var API_KEY = "904143ab-b305-45de-ab8f-adf8a925612e";
 
 app.get('/', function(req, res) {
   console.log("A client connected ... ");
@@ -9,6 +12,31 @@ app.get('/', function(req, res) {
 app.get('/help', function(req, res) {
   res.send('Nothing to display here, yet. Coming soon');
 });
+
+var username
+
+// Subdomain to be used as a callback for Yo.
+app.get('/yo', function(req, res) {
+    username = req.query.username;
+    console.log("Yo received from " + req.query.username);
+    res.send("Received a Yo from " + req.query.username);
+    sendYo(username);
+});
+
+
+function sendYo(username) {
+    request.post('http://api.justyo.co/yo/',
+        { form: { 'api_token': API_KEY,
+              'username': username,
+              'link': 'http://google.com' } },
+        function(error, response, body) {
+            if (!error && response.statuscode == 200) {
+                console.log(body);
+            }
+            console.log("response code = " + response.statuscode);
+        }
+    )
+}
 
 console.log("Server running ...");
 app.listen(3000);
