ca738f0fa2410c28a68efdcc2b5b52a7996ff322
Himanshu Jha <himanshujha19@gmail.com>
2015-02-22 00:14:21 -0500

    Fixing typo. It's statusCode, not statuscode.

--- app.js
+++ app.js
@@ -30,10 +30,9 @@ function sendYo(username) {
               'username': username,
               'link': 'http://google.com' } },
         function(error, response, body) {
-            if (!error && response.statuscode == 200) {
+            if (!error && response.statusCode == 200) {
                 console.log(body);
             }
-            console.log("response code = " + response.statuscode);
         }
     )
 }
