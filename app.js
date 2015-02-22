253fcdd3c3558b45f901807cf0b8e79a8224b5d5
Himanshu Jha <himanshujha19@gmail.com>
2015-02-22 06:23:29 -0500

    Restructuring the code and adding package.json file

--- app.js
+++ app.js
@@ -1,57 +1,22 @@
-var express = require('express')
-var request = require('request')
-var mongoose = require('mongoose')
+var express = require('express'),
+    request = require('request'),
+    mongoose = require('mongoose')
+
 var app = express()
 
-mongoose.connect('mongodb://localhost/test');
-var db = mongoose.connection;
+mongoose.connect('mongodb://localhost/test')
+var db = mongoose.connection
+
+var TIME_INTERVAL = 30000,
+    API_KEY = "904143ab-b305-45de-ab8f-adf8a925612e",
+    username,
+    userSchema,
+    userModel
+
 db.once('open', function(callback) {
     setupDB();
 });
 
-var TIME_INTERVAL = 30000;
-var API_KEY = "904143ab-b305-45de-ab8f-adf8a925612e";
-
-app.get('/', function(req, res) {
-  console.log("A client connected ... ");
-  res.send('Server Started');
-});
-
- 
-app.get('/help', function(req, res) {
-  res.send('Nothing to display here, yet. Coming soon');
-});
-
-var username
-
-// Subdomain to be used as a callback for Yo.
-app.get('/yo', function(req, res) {
-    username = req.query.username;
-    console.log("Yo received from " + req.query.username);
-    res.send("Received a Yo from " + req.query.username);
-    //sendYo(username);
-    writeToDb(username);
-    //getInfoFromDb();
-});
-
-
-function sendYo(username) {
-    request.post('http://api.justyo.co/yo/',
-        { form: { 'api_token': API_KEY,
-              'username': username,
-              'link': 'http://google.com' } },
-        function(error, response, body) {
-            if (!error && response.statusCode == 200) {
-                console.log("Yo sent to  " + username);
-                console.log(body);
-            }
-        }
-    )
-    removeFromDb(username);
-}
-
-var userSchema
-var userModel
 function setupDB() {
     userSchema = mongoose.Schema({
         username : String,
@@ -59,6 +24,43 @@ function setupDB() {
     })
     userModel = mongoose.model('userModel', userSchema)
 }
+
+
+app.get('/', function(req, res) {
+  console.log("A client connected ... ")
+  res.send('Server Started')
+});
+
+ 
+app.get('/help', function(req, res) {
+  res.send('Nothing to display here, yet. Coming soon');
+});
+
+
+// Subdomain to be used as a callback for Yo.
+app.get('/yo', function(req, res) {
+    username = req.query.username;
+    console.log("Yo received from " + username);
+    res.send("Received a Yo from " + username);
+    writeToDb(username);
+});
+
+
+function sendYo(yoUsername) {
+    request.post('http://api.justyo.co/yo/',
+        { form: { 'api_token': API_KEY,
+              'username': yoUsername,
+              'link': 'http://google.com' } },
+        function(error, response, body) {
+            if (!error && response.statusCode == 200) {
+                console.log("Yo sent to  " + yoUsername);
+                console.log(body);
+            }
+        }
+    )
+    removeFromDb(yoUsername);
+}
+
 
 
 function writeToDb(yoUsername) {
@@ -70,6 +72,7 @@ function writeToDb(yoUsername) {
     });
 }
 
+
 function removeFromDb(yoUsername) {
     console.log("Deleting user from Database ...");
     userModel.find({username : yoUsername}).remove(function(err, user) {
@@ -79,6 +82,7 @@ function removeFromDb(yoUsername) {
     
     clearInterval(intVar);
 }
+
 
 function getInfoFromDb() {
     console.log("Reading database...");
@@ -92,12 +96,14 @@ function getInfoFromDb() {
     });
 }
 
+
 function isTimeUp(givenTime) {
     var currentTime = new Date(Date.now());
     if (currentTime - givenTime > TIME_INTERVAL) {
         return true;
     }
 }
+
 
 var intVar = setInterval(getInfoFromDb, 10 * 1000);
 
